3c16d4b92993838ab28e77d6fac33d915de6d8c1
Jesus Arce <chuy_1322@hotmail.com>
2022-05-09 15:04:08 -0600

    Se agrego interfaz para que el usuario ingrese los valores del producto y descuento y se aprendio a desplegar resultado mediante el html utilizando JavaScript

--- descuentos.js
+++ descuentos.js
@@ -12,3 +12,13 @@ function calcularPrecioConDescuento(precio, descuento){
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = Number(document.getElementById("InputPrice").value);
+    const inputDiscount = Number(document.getElementById("InputDiscount").value);
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrice, inputDiscount);
+
+    const resultPrice = document.getElementById("ResultPrice");
+    resultPrice.innerText = "El Precio con descuento es: $" + precioConDescuento;
+}

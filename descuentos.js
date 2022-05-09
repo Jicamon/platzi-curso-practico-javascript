2aad51f224cdccc651d8887f17626f3a73cc4fa6
Jesus Arce <chuy_1322@hotmail.com>
2022-05-09 16:26:19 -0600

    Reto: Integrar cupones en lugar de descuentos variables. Se elimina la opcion de introducir descuentos manualmente y se implementa la utilizacion de cupones fijos, si no se intruduce el cupon correcto, no se aplicara descuento

--- descuentos.js
+++ descuentos.js
@@ -1,11 +1,28 @@
-const precioOriginal = 120;
-const descuento = 18;
-/* console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento,
-}); */
+const cupones = 
+[
+    { 
+        cupon: "DESC10",
+        descuento: 10
+    },
+    { 
+        cupon: "DESC30",
+        descuento: 30
+    },
+    { 
+        cupon: "DESC50",
+        descuento: 50
+    }
+];
+
+function getDiscountValue(key){
+    
+    const discount = cupones.find(x => x.cupon == key);
+    if( discount != undefined ){
+        return discount.descuento;
+    }else{
+        return 0;
+    }
+}
 
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -15,10 +32,13 @@ function calcularPrecioConDescuento(precio, descuento){
 
 function onClickButtonPriceDiscount(){
     const inputPrice = Number(document.getElementById("InputPrice").value);
-    const inputDiscount = Number(document.getElementById("InputDiscount").value);
+    const inputCoupon = document.getElementById("InputCoupon").value;
 
-    const precioConDescuento = calcularPrecioConDescuento(inputPrice, inputDiscount);
+    const discount = getDiscountValue(inputCoupon);
+
+    const precioConDescuento = calcularPrecioConDescuento(inputPrice, discount);
 
     const resultPrice = document.getElementById("ResultPrice");
     resultPrice.innerText = "El Precio con descuento es: $" + precioConDescuento;
 }
+

77b2ee8fb0d9855519f1de646c5067410acd82b8
Jesus Arce <chuy_1322@hotmail.com>
2022-05-12 16:18:41 -0600

    Calcular la mediana del top 10% de los sueldos de la lista proporcionada utilizando el metodo Splice para separar al top 10% de elementos de la lista ordenada previamente

--- analisis.js
+++ analisis.js
@@ -1,16 +1,10 @@
-const salariosMex = mexico.map(
-
-    function (persona) {
-        return persona.salary;
-    }
-
-);
-
-const salariosMexSorted = salariosMex.sort((a,b)=>a-b);
+// Helpers
 
 function esPar(num){
     return (num % 2 === 0);
 }
+
+// Calculadora de mediana
 
 function medianaSalarios(lista){
     const mitad = parseInt(lista.length / 2);
@@ -29,6 +23,30 @@ function medianaSalarios(lista){
     return mediana;
 }
 
-console.log(
-    medianaSalarios(salariosMexSorted)
+// Mediana general
+const salariosMex = mexico.map(
+
+    function (persona) {
+        return persona.salary;
+    }
+
 );
+
+const salariosMexSorted = salariosMex.sort((a,b)=>a-b);
+
+
+const medianaGeneralMex = medianaSalarios(salariosMexSorted);
+
+// Mediana del Top 10%
+
+const spliceStart = parseInt((salariosMexSorted.length * 90 ) / 100);
+const spliceCount = salariosMexSorted.length - spliceStart;
+
+const salariosMexTop10 = salariosMexSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Mex = medianaSalarios(salariosMexTop10);
+
+console.log({
+    medianaGeneralMex,
+    medianaTop10Mex,
+});
